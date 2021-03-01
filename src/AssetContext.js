173ab6844785bec271637c27271c04f52c8449da
Sundara Ram Matta <msram0210@gmail.com>
2021-03-01 00:51:00 -0500

    minor change

--- src/AssetContext.js
+++ src/AssetContext.js
@@ -9,56 +9,72 @@ export const AssetProvider = props => {
       trainerImg: 'lake-inniscarra-trainer.jpg',
       isMetric: true,
       hasMoreData: true,
-      isSelected: false
+      isSelected: false,
+      time: "50:37",
+      distance: 1741
     },
     {
       thumbImg: 'performance-series-thumb.jpg',
       trainerImg: 'performance-series-trainer.jpg',
       isMetric: false,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "NA",
+      distance: null
     },
     {
       thumbImg: 'slow-pulls-thumb.jpg',
       trainerImg: 'slow-pulls-trainer.jpg',
       isMetric: true,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "41:15",
+      distance: 1520
     },
     {
       thumbImg: '20-minutes-to-toned-thumb.jpg',
       trainerImg: '20-minutes-to-toned-trainer.jpg',
       isMetric: false,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "NA",
+      distance: null
     },
     {
       thumbImg: 'charles-race-thumb.jpg',
       trainerImg: 'charles-race-trainer.jpg',
       isMetric: true,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "32:49",
+      distance: 1489
     },
     {
       thumbImg: 'full-body-hiit-thumb.jpg',
       trainerImg: 'full-body-hiit-trainer.jpg',
       isMetric: false,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "NA",
+      distance: null
     },
     {
       thumbImg: 'kafue-river-thumb.jpg',
       trainerImg: 'kafue-river-trainer.jpg',
       isMetric: true,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "35:45",
+      distance: 1896
     },
     {
       thumbImg: 'shred-and-burn-thumb.jpg',
       trainerImg: 'shred-and-burn-trainer.jpg',
       isMetric: false,
       hasMoreData: false,
-      isSelected: false
+      isSelected: false,
+      time: "NA",
+      distance: null
     }
   ]);
 
--- src/Imagecard/MetricInfo/MetricInfo.js
+++ src/Imagecard/MetricInfo/MetricInfo.js
@@ -4,22 +4,21 @@ import AccessAlarmRoundedIcon from '@material-ui/icons/AccessAlarmRounded';
 import DirectionsRunRoundedIcon from '@material-ui/icons/DirectionsRunRounded';
 
 function metricInfo({metricInfo}) {
-
-  function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+  // We can also use randomNumber function to gnerate values here for time and distance
+  // function randomNumber(min, max) {
+  //   return Math.floor(Math.random() * (max - min) + min);
+  // }
 
   function displayMetrics(imagePath) {
     if (imagePath.isMetric) {
       return (
         <div className="card-container__info__metrics">
-          <span><AccessAlarmRoundedIcon className="metric_icon" fontSize="small" />{randomNumber(30,60)}:{randomNumber(30,60)}</span>
-          <span><DirectionsRunRoundedIcon className="metric_icon" fontSize="small" />{randomNumber(1000,10000)} m</span>
+          <span><AccessAlarmRoundedIcon className="metric_icon" fontSize="small" />{`${imagePath.time}`}</span>
+          <span><DirectionsRunRoundedIcon className="metric_icon" fontSize="small" />{`${imagePath.distance}`} m</span>
         </div>
       );
     }
   }
-
 
   return (
     <div>
